5d080e348247f3c5dafa22d473cb5d11a5d928cd
Ndeye456 <tndeyeamie456@gmail.com>
2023-09-23 14:20:39 +0000

    first commit

---
+++ session.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+// Vérifier si l'utilisateur est authentifié
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+// Par exemple, vous pouvez afficher le nom de l'utilisateur authentifié :
+    echo "Bienvenue, " . $_SESSION["user_id"] . "!";
+?>
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page à accès limité</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        .dashboard-container {
+            max-width: 800px;
+            margin: 50px auto;
+            background-color: #fff;
+            padding: 20px 30px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+        h2 {
+            color: #333;
+        }
+        p {
+            color: #555;
+        }
+        a {
+            color: #007BFF;
+            text-decoration: none;
+            list-style: none;
+        }
+        li  {
+            list-style: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+</head>
+<body>
+    <div class="dashboard-container">
+    <h1>Bienvenue sur la page à accès limité</h1>
+    <p>Ceci est une page réservée aux utilisateurs authentifiés.</p>
+    <p>  <ul>
+        <li><a href="ajout_produit.php">Ajouter un Produit</a></li>
+        <li><a href="transaction.php">transaction</a></li>
+        <li><a href="liste_transaction.php">liste _transaction</a></li>
+        <li><a href="verifier_stock.php">Vérifier le Stock</a></li>
+    </ul.</p>
+    <p><a href="deconnexion.php">Se déconnecter</a></p>
+    <div>
+</body>
+</html>
